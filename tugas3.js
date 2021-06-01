3040c42afe5d448869949f03e8593d014153c7f0
EffenrilAgung <66568136+EffenrilAgung@users.noreply.github.com>
2021-06-01 20:43:07 +0700

    Initial commit

---
+++ tugas3.js
@@ -0,0 +1,11 @@
+function nilaibalik() {
+    var nilai = "hasil"
+    return nilai
+}
+
+const parameter = function (x, y, z) {
+    return x, y, z
+}
+
+console.log(nilaibalik())
+console.log("hai", "apa", "kabar")
